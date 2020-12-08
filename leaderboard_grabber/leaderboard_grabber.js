3002cfe9a0c8843e973b52e5a35af7b8cede6bc5
Ioan Clarke <clarkei@cardiff.ac.uk>
2020-12-08 11:25:18 +0000

    untracked test.txt files and leaderboard_grabber

--- leaderboard_grabber/leaderboard_grabber.js
+++
@@ -1,9 +0,0 @@
-let requestURL = 'https://adventofcode.com/2020/leaderboard/private/view/1039209.json';
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-  const response = request.response;
-  console.log(response)
-}
