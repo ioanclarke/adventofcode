d8b790a83b00bc17dac14122349f12324cf5f4de
Ioan Clarke <clarkei@cardiff.ac.uk>
2020-12-23 20:39:31 +0000

    added solution to b using array instead of dict

--- day-23/puzzle23b(faster).py
+++ day-23/puzzle23b(faster).py
@@ -2,9 +2,9 @@
 #389125467
 input = '792845136'
 
-node = {}
+node = [0]*1000001
+node[0] = [0]
 for i in range(1, len(input) - 1):
-    #print(i, input[i])
     node[int(input[i])] = int(input[i+1])
 
 node[int(input[0])] = int(input[1])
@@ -13,19 +13,17 @@ node[int(input[-1])] = 10
 node[10] = 11
 node[1000000] = int(input[0])
 for i in range(len(input) + 2, 1000000):
-    node[i] = i+1
+    node[i] = i + 1
 
 curr = int(input[0])
 
-for i in range(1000000):
-    #print(f'current: {curr}')
+for i in range(10000000):
     next1 = node[curr]
     next2 = node[node[curr]]
     next3 = node[node[node[curr]]]
     curr_prev = node[curr]
     next3_next = node[next3]
     next3_next_next = node[next3_next]
-    #print(f'pick up: {move}')
 
     dest = curr - 1
     if dest == 0:
@@ -34,7 +32,6 @@ for i in range(1000000):
         dest -= 1
         if dest == 0:
             dest = 1000000
-    #print(f'destination: {dest}')
 
     dest_next = node[dest]
     dest_next_next = node[dest_next]
@@ -47,13 +44,13 @@ for i in range(1000000):
     node[next3_next] = next3_next_next
     node[dest] = next1
 
-    #curr.next always becomes next3_next
-    #next3_next.next doesn't change unless next3_next is dest
-    #dest.next always becomes next1
-    #dest_next.next doesn't change
-    #next1.next always becomes next2
-    #next2.next doesn't change
-    #next3.next always becomes dest_next
+    #node[curr] always becomes next3_next
+    #node[next3_next] doesn't change unless next3_next is dest
+    #node[dest] always becomes next1
+    #node[dest_next] doesn't change
+    #node[next1] always becomes next2
+    #node[next2] doesn't change
+    #node[next3] always becomes dest_next
 
     curr = node[curr]
 
